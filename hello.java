c0ac924070170c34e81c2baa637eb3acf98b7d37
Chor Kiat Chu <chuchorkiat@gmail.com>
2019-12-25 22:56:06 +0800

    Add extra statement

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String args[]) {
     System.out.println("Hello World");
-    //testing
+    System.out.println("Github functions can be done here");
   }
 }
